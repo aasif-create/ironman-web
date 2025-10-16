b5a9c7ac8e917f30f0aa7b29788c6765011f8513
aasif-create <aasifmmd12345@gmail.com>
2025-10-16 21:34:33 +0530

    UPDATED

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('welcomeBtn');
 btn.addEventListener('click', () => {
   btn.classList.add('pop');
-  setTimeout(() => { location.href = 'home.html'; }, 250);
+  setTimeout(() => { location.href = 'home.html'; }, 550);
 });
 btn.addEventListener('keydown', (e) => {
   if (e.key === 'Enter' || e.key === ' ') {
